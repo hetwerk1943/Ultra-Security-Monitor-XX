4f9fae29b9608a91922d33c46f4945babcc701dc
AlgoMaster <219596166+hetwerk1943@users.noreply.github.com>
2026-03-02 19:24:08 +0100

    Create EntropyEngine.ps1
    Signed-off-by: AlgoMaster <219596166+hetwerk1943@users.noreply.github.com>

---
+++ 📂 RTT-Shield/EntropyEngine.ps1
@@ -0,0 +1,11 @@
+-Shield/EntropyEngine.ps1
+
+Entropy Injection Engine
+
+Silnik odpowiedzialny za:
+
+Generowanie losowych opóźnień
+
+Symulację naturalnej nieregularności sieci
+
+Maskowanie charakterystycznych wzorców odpowiedzi
